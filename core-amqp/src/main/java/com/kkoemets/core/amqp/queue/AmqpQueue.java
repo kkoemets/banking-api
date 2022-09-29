a2a8122ae8c076e027eb541943ee8807c39afd9c
kkoemets <koemets.kristjan@gmail.com>
2022-09-29 23:14:44 +0300

    Send queue message on transaction commit

--- core-amqp/src/main/java/com/kkoemets/core/amqp/queue/AmqpQueue.java
+++ core-amqp/src/main/java/com/kkoemets/core/amqp/queue/AmqpQueue.java
@@ -4,6 +4,7 @@ import com.kkoemets.core.amqp.message.AmqpMessage;
 import org.slf4j.Logger;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
@@ -13,6 +14,7 @@ public abstract class AmqpQueue<C extends AmqpMessage> {
     @Autowired
     private RabbitTemplate template;
 
+    @Transactional
     public void send(C message) {
         template.convertAndSend(getQueueName(), message);
         log.info("Sent message to queue-{}", getQueueName());
--- daemon/src/main/java/com/kkoemets/daemon/listener/AmqpListener.java
+++ daemon/src/main/java/com/kkoemets/daemon/listener/AmqpListener.java
@@ -1,12 +1,16 @@
 package com.kkoemets.daemon.listener;
 
 import com.kkoemets.core.amqp.message.AmqpMessage;
+import org.slf4j.Logger;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.core.MessageListener;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import static org.slf4j.LoggerFactory.getLogger;
+
 public abstract class AmqpListener<C extends AmqpMessage> implements MessageListener {
+    private static final Logger log = getLogger(AmqpListener.class);
 
     @Autowired
     private MessageConverter converter;
@@ -16,6 +20,8 @@ public abstract class AmqpListener<C extends AmqpMessage> implements MessageList
     @Override
     @SuppressWarnings("unchecked")
     public void onMessage(Message message) {
+        log.info("{} received message-{}",
+                message.getMessageProperties().getConsumerQueue(), new String(message.getBody()));
         onMessage((C) converter.fromMessage(message));
     }
 
