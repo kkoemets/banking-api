c8c647638c1821fe061b34efded7e74e967b11ec
kkoemets <koemets.kristjan@gmail.com>
2022-10-02 16:46:44 +0300

    Add correlation id to queue messages

--- core-amqp/src/main/java/com/kkoemets/core/amqp/queue/AmqpQueue.java
+++ core-amqp/src/main/java/com/kkoemets/core/amqp/queue/AmqpQueue.java
@@ -2,6 +2,7 @@ package com.kkoemets.core.amqp.queue;
 
 import com.kkoemets.core.amqp.message.AmqpMessage;
 import org.slf4j.Logger;
+import org.slf4j.MDC;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,7 +17,11 @@ public abstract class AmqpQueue<C extends AmqpMessage> {
 
     @Transactional
     public void send(C message) {
-        template.convertAndSend(getQueueName(), message);
+        template.convertAndSend(getQueueName(), message, m -> {
+            m.getMessageProperties().setCorrelationId(MDC.get("requestId"));
+            return m;
+        });
+
         log.info("Sent message to queue-{}", getQueueName());
     }
 
--- daemon/src/main/java/com/kkoemets/daemon/listener/AmqpListener.java
+++ daemon/src/main/java/com/kkoemets/daemon/listener/AmqpListener.java
@@ -2,15 +2,19 @@ package com.kkoemets.daemon.listener;
 
 import com.kkoemets.core.amqp.message.AmqpMessage;
 import org.slf4j.Logger;
+import org.slf4j.MDC;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.core.MessageListener;
+import org.springframework.amqp.core.MessageProperties;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import static java.util.UUID.randomUUID;
 import static org.slf4j.LoggerFactory.getLogger;
 
 public abstract class AmqpListener<C extends AmqpMessage> implements MessageListener {
     private static final Logger log = getLogger(AmqpListener.class);
+    private static final int REQUEST_ID_LENGTH = 7;
 
     @Autowired
     private MessageConverter converter;
@@ -20,9 +24,18 @@ public abstract class AmqpListener<C extends AmqpMessage> implements MessageList
     @Override
     @SuppressWarnings("unchecked")
     public void onMessage(Message message) {
-        log.info("{} received message-{}",
-                message.getMessageProperties().getConsumerQueue(), new String(message.getBody()));
-        onMessage((C) converter.fromMessage(message));
+        MDC.put("requestId", randomUUID().toString().substring(0, REQUEST_ID_LENGTH));
+
+        try {
+            MessageProperties messageProperties = message.getMessageProperties();
+
+            log.info("{} received message-{} correlationId-{}", messageProperties.getConsumerQueue(),
+                    new String(message.getBody()), messageProperties.getCorrelationId());
+
+            onMessage((C) converter.fromMessage(message));
+        } finally {
+            MDC.clear();
+        }
     }
 
 }
