a73e2502a0b7bdeb6b3fcc2ec5a104cbf65640ee
kkoemets <koemets.kristjan@gmail.com>
2022-10-10 20:43:52 +0300

    Create audit tables

--- core/src/integration-test/java/com/kkoemets/core/service/AccountServiceIntegTest.java
+++ core/src/integration-test/java/com/kkoemets/core/service/AccountServiceIntegTest.java
@@ -4,6 +4,8 @@ import com.kkoemets.core.CoreIntegTest;
 import com.kkoemets.domain.id.CustomerId;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
 
 import static com.kkoemets.domain.codes.CountryIsoCode2.EE;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -20,6 +22,8 @@ public class AccountServiceIntegTest extends CoreIntegTest {
     }
 
     @Test
+    @Transactional
+    @Rollback(value = false)
     public void shouldExecute() {
         service.add(new AddAccountDto(service.getNextSeqValue(), new CustomerId(1L), EE));
     }
--- docker/postgres/sql/init.sql
+++ docker/postgres/sql/init.sql
@@ -8,6 +8,15 @@ BEGIN
 
     new.modified_timestamp := CURRENT_TIMESTAMP;
     new.modified_session_id := CURRENT_SETTING('app.session_id');
+
+    -- https://dba.stackexchange.com/questions/58455/insert-record-into-a-table-in-a-trigger-function
+    -- Actually, you do not have to expand the record manually.
+    -- As long as number, sequence and types of columns match between the two tables,
+    -- you can use this much simpler form:
+    EXECUTE 'INSERT INTO ' || tg_table_schema::TEXT || '_audit' || '.' || tg_table_name::TEXT ||
+            '_audit' ||
+            ' SELECT ' || QUOTE_LITERAL(CLOCK_TIMESTAMP()) || 'AS audit_timestamp, ' || QUOTE_LITERAL(tg_op::TEXT) ||
+            ' AS event, ($1).*' USING new;
     RETURN new;
 END;
 $audit_trigger$ LANGUAGE plpgsql;
@@ -17,6 +26,7 @@ CREATE ROLE core_usr
     PASSWORD 'core_pw';
 
 CREATE SCHEMA core;
+CREATE SCHEMA core_audit;
 
 CREATE TABLE core.account
 (
@@ -27,9 +37,9 @@ CREATE TABLE core.account
     inserted_timestamp  TIMESTAMP WITH TIME ZONE NOT NULL,
 
     created_timestamp   TIMESTAMP WITH TIME ZONE NOT NULL,
-    created_session_id  VARCHAR(255),
+    created_session_id  VARCHAR(255)             NOT NULL,
     modified_timestamp  TIMESTAMP WITH TIME ZONE NOT NULL,
-    modified_session_id VARCHAR(255)
+    modified_session_id VARCHAR(255)             NOT NULL
 );
 
 CREATE SEQUENCE core.acco_seq AS
@@ -44,6 +54,23 @@ CREATE TRIGGER account_audit_trigger
     FOR EACH ROW
 EXECUTE FUNCTION audit_trigger();
 
+CREATE TABLE core_audit.account_audit
+(
+    audit_timestamp     TIMESTAMP WITH TIME ZONE,
+    event               VARCHAR(6),
+
+    acco_id             BIGINT,
+    cust_id             BIGINT,
+    status_code         VARCHAR(1),
+    country             VARCHAR(2),
+    inserted_timestamp  TIMESTAMP WITH TIME ZONE,
+
+    created_timestamp   TIMESTAMP WITH TIME ZONE,
+    created_session_id  VARCHAR(255),
+    modified_timestamp  TIMESTAMP WITH TIME ZONE,
+    modified_session_id VARCHAR(255)
+);
+
 CREATE TABLE core.balance
 (
     bala_id             BIGINT PRIMARY KEY,
@@ -53,9 +80,9 @@ CREATE TABLE core.balance
     valid_to_timestamp  TIMESTAMP WITH TIME ZONE,
 
     created_timestamp   TIMESTAMP WITH TIME ZONE NOT NULL,
-    created_session_id  VARCHAR(255),
+    created_session_id  VARCHAR(255)             NOT NULL,
     modified_timestamp  TIMESTAMP WITH TIME ZONE NOT NULL,
-    modified_session_id VARCHAR(255)
+    modified_session_id VARCHAR(255)             NOT NULL
 );
 
 CREATE SEQUENCE core.bala_seq AS
@@ -63,6 +90,22 @@ CREATE SEQUENCE core.bala_seq AS
     START WITH 1000;
 
 GRANT SELECT, INSERT, UPDATE ON core.balance TO core_usr;
+CREATE TABLE core_audit.balance_audit
+(
+    audit_timestamp     TIMESTAMP WITH TIME ZONE,
+    event               VARCHAR(6),
+
+    bala_id             BIGINT,
+    acco_id             BIGINT,
+    amount              NUMERIC(16, 2),
+    ccy                 VARCHAR(3),
+    valid_to_timestamp  TIMESTAMP WITH TIME ZONE,
+
+    created_timestamp   TIMESTAMP WITH TIME ZONE,
+    created_session_id  VARCHAR(255),
+    modified_timestamp  TIMESTAMP WITH TIME ZONE,
+    modified_session_id VARCHAR(255)
+);
 
 CREATE TRIGGER balance_audit_trigger
     BEFORE INSERT OR UPDATE
@@ -76,8 +119,22 @@ CREATE TABLE core.allowed_currency
     enabled             BOOLEAN                  NOT NULL,
 
     created_timestamp   TIMESTAMP WITH TIME ZONE NOT NULL,
-    created_session_id  VARCHAR(255),
+    created_session_id  VARCHAR(255)             NOT NULL,
     modified_timestamp  TIMESTAMP WITH TIME ZONE NOT NULL,
+    modified_session_id VARCHAR(255)             NOT NULL
+);
+
+CREATE TABLE core_audit.allowed_currency_audit
+(
+    audit_timestamp     TIMESTAMP WITH TIME ZONE,
+    event               VARCHAR(6),
+
+    ccy                 VARCHAR(3),
+    enabled             BOOLEAN,
+
+    created_timestamp   TIMESTAMP WITH TIME ZONE,
+    created_session_id  VARCHAR(255),
+    modified_timestamp  TIMESTAMP WITH TIME ZONE,
     modified_session_id VARCHAR(255)
 );
 
@@ -86,6 +143,7 @@ CREATE TRIGGER allowed_currency_audit_trigger
     ON core.allowed_currency
     FOR EACH ROW
 EXECUTE FUNCTION audit_trigger();
+
 GRANT SELECT ON core.balance TO core_usr;
 
 SELECT SET_CONFIG('app.session_id', 'init.sql', FALSE);
@@ -107,8 +165,28 @@ CREATE TABLE core.transaction
     inserted_timestamp  TIMESTAMP WITH TIME ZONE NOT NULL,
 
     created_timestamp   TIMESTAMP WITH TIME ZONE NOT NULL,
-    created_session_id  VARCHAR(255),
+    created_session_id  VARCHAR(255)             NOT NULL,
     modified_timestamp  TIMESTAMP WITH TIME ZONE NOT NULL,
+    modified_session_id VARCHAR(255)             NOT NULL
+);
+
+CREATE TABLE core_audit.transaction_audit
+(
+    audit_timestamp     TIMESTAMP WITH TIME ZONE,
+    event               VARCHAR(6),
+
+    tran_id             BIGINT,
+    acco_id             BIGINT,
+    amount              NUMERIC(16, 2),
+    ccy                 VARCHAR(3),
+    direction           VARCHAR(3),
+    description         VARCHAR(140),
+    status              VARCHAR(3),
+    inserted_timestamp  TIMESTAMP WITH TIME ZONE,
+
+    created_timestamp   TIMESTAMP WITH TIME ZONE,
+    created_session_id  VARCHAR(255),
+    modified_timestamp  TIMESTAMP WITH TIME ZONE,
     modified_session_id VARCHAR(255)
 );
 
